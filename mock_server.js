d808e343f380672dc8703cecefc06d4bcf94db17
GaryGai <gaisanshi@hotmail.com>
2015-06-21 22:06:53 -0700

    Update mock_server.js

--- mock_server.js
+++ mock_server.js
@@ -1,23 +1,11 @@
 /**
- * Copyright [2015] [Gary Gai]
  *
- * Version: 0.0.1
- * 
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- *
+ * Created by Gary Gai on 6/10/15.
  *
  *
  * Generic Mock Server by Phantom JS
+ *
+ * Version: 0.0.1
  *
  * This Module will start a http server and bind with the given port. After that, it accept to config the request/response mapping with
  *  method 'when(request)' and 'response(response)'. If you want to re-config the mapping, method 'reset()' should be called to clear the exiting mapping,
